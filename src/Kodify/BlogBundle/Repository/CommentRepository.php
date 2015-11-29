580900a461ef091f4238ad40e0c2198db7beab41
Adrián Ríos <adrian.web.dev@gmail.com>
2015-11-29 16:28:17 +0100

    Add Comment repository and service

---
+++ src/Kodify/BlogBundle/Repository/CommentRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adrianr
+ * Date: 29/11/15
+ * Time: 1:41
+ */
+
+namespace Kodify\BlogBundle\Repository;
+
+
+class CommentRepository extends AbstractBaseRepository
+{
+
+
+
+}
