cdad89fa65ec4118940173f4bca3a234ba2df978
Adrián Ríos <adrian.web.dev@gmail.com>
2015-11-30 19:53:00 +0100

    Change message

--- src/Kodify/BlogBundle/Model/Handler/Exception/CommandHandlerException.php
+++ src/Kodify/BlogBundle/Model/Handler/Exception/CommandHandlerException.php
@@ -5,5 +5,5 @@ namespace Kodify\BlogBundle\Model\Handler\Exception;
 
 class CommandHandlerException extends \Exception
 {
-    public $message = 'An exception occurs while handling command';
+    public $message = 'Exception handling command';
 }
