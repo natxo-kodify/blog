97d6014042552d7a61c42d3e897b285b5566f134
Sergi Rodríguez <sergirt91@hotmail.com>
2016-03-19 17:05:27 +0100

    Added Entity Comment

---
+++ src/Kodify/BlogBundle/Entity/Comment.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Kodify\BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Comment
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Kodify\BlogBundle\Repository\CommentRepository")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Comment extends AbstractBaseEntity
+{
+	 /**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="text", type="text")
+	 * @Assert\NotBlank()
+	 *
+	 */
+	private $text;
+
+	/**
+	 * @Assert\NotBlank()
+	 * @ORM\ManyToOne(targetEntity="Author", inversedBy="comments")
+	 * @ORM\JoinColumn(name="authorId", referencedColumnName="id")
+	 */
+	protected $author;
+
+	/**
+	 * Get id
+	 *
+	 * @return integer
+	 */
+	public function getId()
+	{
+	    return $this->id;
+	}
+
+	/**
+	 * Set text
+	 *
+	 * @param string $text
+	 * @return Comment
+	 */
+	public function setText($text)
+	{
+	    $this->text = $text;
+
+	    return $this;
+	}
+
+	/**
+	 * Get text
+	 *
+	 * @return string
+	 */
+	public function getText()
+	{
+	    return $this->text;
+	}
+
+	/**
+	 * Set author
+	 *
+	 * @param \Kodify\BlogBundle\Entity\Author $author
+	 * @return Comment
+	 */
+	public function setAuthor(\Kodify\BlogBundle\Entity\Author $author = null)
+	{
+	    $this->author = $author;
+
+	    return $this;
+	}
+
+	/**
+	 * Get author
+	 *
+	 * @return \Kodify\BlogBundle\Entity\Author
+	 */
+	public function getAuthor()
+	{
+	    return $this->author;
+	}
+}
