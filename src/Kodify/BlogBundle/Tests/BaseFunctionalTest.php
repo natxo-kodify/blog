002864f43930d4e638137b668479128cadf1ca92
Jesús Díez <jesus@jesusdiez.com>
2016-04-13 18:28:41 +0200

    Require PHPUnit in dev, remove config param to avoid warning. Fix test base class to make tests green.

--- src/Kodify/BlogBundle/Tests/BaseFunctionalTest.php
+++ src/Kodify/BlogBundle/Tests/BaseFunctionalTest.php
@@ -4,7 +4,7 @@ namespace Kodify\BlogBundle\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class BaseFunctionalTest extends WebTestCase
+abstract class BaseFunctionalTest extends WebTestCase
 {
     protected $entityManager;
     protected $client;
