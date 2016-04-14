6f5e2c96f25ca5018cad7bf88a579c3bf3e3c5d8
Jesús Díez <jesus@jesusdiez.com>
2016-04-14 19:18:38 +0200

    Comments feature step definitions.

--- src/Kodify/BlogBundle/Test/Behat/FeatureContext.php
+++ src/Kodify/BlogBundle/Test/Behat/FeatureContext.php
@@ -2,6 +2,8 @@
 
 namespace Kodify\BlogBundle\Test\Behat;
 
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\RawMinkContext;
 use Behat\Symfony2Extension\Context\KernelDictionary;
 
@@ -27,5 +29,93 @@ class FeatureContext extends RawMinkContext
         $em = $this->getService('doctrine.orm.entity_manager');
 
         return $em->getRepository($className);
+    }
+
+    /**
+     * @Given the following authors exist:
+     */
+    public function theFollowingAuthorsExist(TableNode $table)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given the following posts exist:
+     */
+    public function theFollowingPostsExist(TableNode $table)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given the following comments exist:
+     */
+    public function theFollowingCommentsExist(TableNode $table)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given I visit the page for the post with title ":argument"
+     */
+    public function iVisitThePageForThePostWithTitle($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should see a message saying there are no comments
+     */
+    public function iShouldSeeAMessageSayingThereAreNoComments()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should see a comments section with :number comment
+     */
+    public function iShouldSeeACommentsSectionWithComment($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given The comment I see says ":argument"
+     */
+    public function theCommentISeeSays($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given I don't see the comment ":argument"
+     */
+    public function iDonTSeeTheComment($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given I fill the form with ":argument"
+     */
+    public function iFillTheFormWith($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I click on the button ":argument"
+     */
+    public function iClickOnTheButton($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then a comment should be created for the post with the provided data
+     */
+    public function aCommentShouldBeCreatedForThePostWithTheProvidedData()
+    {
+        throw new PendingException();
     }
 }
