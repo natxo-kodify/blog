ba3368d1795f017c4bfba2c59767f005330cb0db
Jesús Díez <jesus@jesusdiez.com>
2016-04-15 21:15:21 +0200

    Ratings feature. Routing, controller and view generation to allowing rate a post

--- src/Kodify/BlogBundle/Controller/PostsController.php
+++ src/Kodify/BlogBundle/Controller/PostsController.php
@@ -4,8 +4,10 @@ namespace Kodify\BlogBundle\Controller;
 
 use Kodify\BlogBundle\Entity\Comment;
 use Kodify\BlogBundle\Entity\Post;
+use Kodify\BlogBundle\Entity\Rating;
 use Kodify\BlogBundle\Form\Type\CommentType;
 use Kodify\BlogBundle\Form\Type\PostType;
+use Kodify\BlogBundle\Form\Type\RatingType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,11 +17,11 @@ class PostsController extends Controller
 {
     public function indexAction()
     {
-        $posts      = $this->getDoctrine()->getRepository('KodifyBlogBundle:Post')->latest();
-        $template   = 'KodifyBlogBundle:Post:List/empty.html.twig';
+        $posts = $this->getDoctrine()->getRepository('KodifyBlogBundle:Post')->latest();
+        $template = 'KodifyBlogBundle:Post:List/empty.html.twig';
         $parameters = ['breadcrumbs' => ['home' => 'Home']];
         if (count($posts)) {
-            $template            = 'KodifyBlogBundle:Post:List/index.html.twig';
+            $template = 'KodifyBlogBundle:Post:List/index.html.twig';
             $parameters['posts'] = $posts;
         }
 
@@ -29,11 +31,14 @@ class PostsController extends Controller
     public function viewAction($id)
     {
         $currentPost = $this->loadPost($id);
-        $form = $this->generateCommentForm($id);
+        $commentForm = $this->generateCommentForm($id);
+        $ratingForm = $this->generateRatingForm($id);
+
         $parameters = [
             'breadcrumbs' => ['home' => 'Home'],
             'post' => $currentPost,
-            'form' => $form->createView(),
+            'comment_form' => $commentForm->createView(),
+            'rating_form' => $ratingForm->createView(),
         ];
 
         /** @var Session $session */
@@ -47,7 +52,7 @@ class PostsController extends Controller
 
     public function createAction(Request $request)
     {
-        $form       = $this->createForm(
+        $form = $this->createForm(
             new PostType(),
             new Post(),
             [
@@ -55,8 +60,9 @@ class PostsController extends Controller
                 'method' => 'POST',
             ]
         );
+
         $parameters = [
-            'form'        => $form->createView(),
+            'form' => $form->createView(),
             'breadcrumbs' => ['home' => 'Home', 'create_post' => 'Create Post']
         ];
 
@@ -71,21 +77,44 @@ class PostsController extends Controller
         return $this->render('KodifyBlogBundle:Default:create.html.twig', $parameters);
     }
 
+    public function addRatingAction($postId, Request $request)
+    {
+        $currentPost = $this->loadPost($postId);
+
+        $ratingForm = $this->generateRatingForm($postId);
+        $ratingForm->handleRequest($request);
+
+        if ($ratingForm->isValid()) {
+            $rating = $ratingForm->getData();
+            $rating->setPost($currentPost);
+            $this->getDoctrine()->getManager()->persist($rating);
+            $this->getDoctrine()->getManager()->flush();
+
+            $session = $this->get('session');
+            $session->getFlashBag()->set('post_message', 'Rating added!');
+        }
+
+        return $this->redirectToRoute('view_post', ['id' => $postId]);
+    }
+
     public function addCommentAction($postId, Request $request)
     {
         $currentPost = $this->loadPost($postId);
 
-        $form = $this->generateCommentForm($postId);
-        $form->handleRequest($request);
+        $ratingForm = $this->generateRatingForm($postId);
+
+        $commentForm = $this->generateCommentForm($postId);
+        $commentForm->handleRequest($request);
 
         $parameters = [
             'breadcrumbs' => ['home' => 'Home'],
             'post' => $currentPost,
-            'form' => $form->createView(),
+            'comment_form' => $commentForm->createView(),
+            'rating_form' => $ratingForm->createView(),
         ];
 
-        if ($form->isValid()) {
-            $comment = $form->getData();
+        if ($commentForm->isValid()) {
+            $comment = $commentForm->getData();
             $comment->setPost($currentPost);
             $this->getDoctrine()->getManager()->persist($comment);
             $this->getDoctrine()->getManager()->flush();
@@ -110,6 +139,22 @@ class PostsController extends Controller
             new Comment(),
             [
                 'action' => $this->generateUrl('add_comment', ['postId' => $postId]),
+                'method' => 'POST',
+            ]
+        );
+    }
+
+    /**
+     * @param int $postId
+     * @return Form
+     */
+    private function generateRatingForm($postId)
+    {
+        return $this->createForm(
+            new RatingType(),
+            new Rating(),
+            [
+                'action' => $this->generateUrl('add_rating', ['postId' => $postId]),
                 'method' => 'POST',
             ]
         );
